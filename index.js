38d4e9c6ad64c23a245e4ba93a8af7c4260733c6
Ibra-Hud <ibrahimhudson61@gmail.com>
2024-10-02 13:36:26 -0400

    multiplesThree

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ const fiveToOneHundred = () => {
     }
 }
 
-fiveToOneHundred()
+// fiveToOneHundred()
+
+const multiplesOfThree = () => {
+
+    for (let i = 0; i <= 100; i++){
+        if (i % 3 === 0){
+            console.log(i)
+        }
+    }
+}
+
+// multiplesOfThree()
